29c6a5cc56140f2c7e6cb8a2311fb6d29ea21bbb
captainyarr <captainyarr42@gmail.com>
2017-04-01 10:37:16 -0700

    Update trakt.tv Client ID, and Secret

--- src/app/lib/providers/watchlist.js
+++ src/app/lib/providers/watchlist.js
@@ -1,13 +1,13 @@
 /* globals moment*/
-(function (App) {
+(function(App) {
     'use strict';
     var Q = require('q');
     var TVApi = App.Providers.get('TVApi');
 
-    var Watchlist = function () {};
+    var Watchlist = function() {};
     Watchlist.prototype.constructor = Watchlist;
 
-    var queryTorrents = function (filters) {
+    var queryTorrents = function(filters) {
         var deferred = Q.defer();
         var now = moment();
 
@@ -15,7 +15,7 @@
         App.db.getSetting({
                 key: 'watchlist-fetched'
             })
-            .then(function (doc) {
+            .then(function(doc) {
                 if (doc) {
                     var d = moment.unix(doc.value);
 
@@ -39,29 +39,31 @@
             App.db.getSetting({
                     key: 'watchlist'
                 })
-                .then(function (doc) {
+                .then(function(doc) {
                     if (doc && !update) {
                         // Returning cached watchlist
                         deferred.resolve(doc.value || []);
                     } else {
                         win.info('Watchlist - Fetching new watchlist');
                         App.Trakt.calendars.myShows(moment().subtract(30, 'days').format('YYYY-MM-DD'), 30)
-                            .then(function (data) {
+                            .then(function(data) {
+                                win.debug("trakt watchlist data: " + data);
                                 App.db.writeSetting({
                                         key: 'watchlist',
                                         value: data
                                     })
-                                    .then(function () {
+                                    .then(function() {
+                                        win.debug("trakt watchlist-fetched data: " + data);
                                         App.db.writeSetting({
                                             key: 'watchlist-fetched',
                                             value: now.unix()
                                         });
                                     })
-                                    .then(function () {
+                                    .then(function() {
                                         deferred.resolve(data || []);
                                     });
                             })
-                            .catch(function (error) {
+                            .catch(function(error) {
                                 deferred.reject(error);
                             });
                     }
@@ -71,10 +73,10 @@
         return deferred.promise;
     };
 
-    var filterShows = function (items) {
+    var filterShows = function(items) {
         var filtered = [];
 
-        items.forEach(function (show) {
+        items.forEach(function(show) {
             var deferred = Q.defer();
 
             if (show.show_id && show.season !== 0) {
@@ -83,7 +85,7 @@
                         season: show.season.toString(),
                         episode: show.episode.toString()
                     }))
-                    .then(function (data) {
+                    .then(function(data) {
                         if (data != null && data.length > 0) {
                             deferred.resolve(null);
                         } else {
@@ -100,10 +102,10 @@
         return Q.all(filtered);
     };
 
-    var formatForPopcorn = function (items) {
+    var formatForPopcorn = function(items) {
         var showList = [];
 
-        items.forEach(function (show) {
+        items.forEach(function(show) {
             if (show === null) {
                 return;
             }
@@ -111,38 +113,38 @@
             var deferred = Q.defer();
             //Try to find it on the shows database and attach the next_episode info
             Database.getTVShowByImdb(show.show_id)
-                .then(function (data) {
+                .then(function(data) {
                     if (data != null) {
                         data.type = 'show';
                         data.image = data.images.poster;
                         data.imdb = data.imdb_id;
                         data.next_episode = show.next_episode;
                         // Fallback for old bookmarks without provider in database or marked as Eztv
-                        if (typeof (data.provider) === 'undefined' || data.provider === 'Eztv') {
+                        if (typeof(data.provider) === 'undefined' || data.provider === 'Eztv') {
                             data.provider = 'TVApi';
                         }
                         deferred.resolve(data);
                     } else {
                         //If not found, then get the details from TVApi and add it to the DB
                         data = TVApi.detail(show.show_id, show, false)
-                            .then(function (data) {
+                            .then(function(data) {
                                 if (data) {
                                     data.provider = 'TVApi';
                                     data.type = 'show';
                                     data.next_episode = show.next_episode;
 
                                     Database.addTVShow(data)
-                                        .then(function (idata) {
+                                        .then(function(idata) {
                                             deferred.resolve(data);
                                         })
-                                        .catch(function (err) {
+                                        .catch(function(err) {
                                             deferred.resolve(null);
                                         });
                                 } else {
                                     deferred.resolve(null);
                                 }
                             })
-                            .catch(function (error) {
+                            .catch(function(error) {
                                 deferred.resolve(null);
                             });
                     }
@@ -153,15 +155,15 @@
         return Q.all(showList);
     };
 
-    Watchlist.prototype.extractIds = function (items) {
+    Watchlist.prototype.extractIds = function(items) {
         return _.pluck(items, 'imdb_id');
     };
 
-    Watchlist.prototype.fetch = function (filters) {
+    Watchlist.prototype.fetch = function(filters) {
         return queryTorrents(filters)
             .then(filterShows)
             .then(formatForPopcorn)
-            .then(function (shows) {
+            .then(function(shows) {
                 return {
                     results: _.filter(shows, Boolean),
                     hasMore: false
@@ -169,32 +171,32 @@
             });
     };
 
-    Watchlist.prototype.detail = function (torrent_id, old_data, callback) {
+    Watchlist.prototype.detail = function(torrent_id, old_data, callback) {
         return TVApi.detail(torrent_id, old_data, callback);
     };
 
-    Watchlist.prototype.fetchWatchlist = function () {
+    Watchlist.prototype.fetchWatchlist = function() {
         var now = moment();
         var deferred = Q.defer();
 
         win.info('Watchlist - Fetching new watchlist');
         App.Trakt.calendars.myShows(moment().subtract(30, 'days').format('YYYY-MM-DD'), 30)
-            .then(function (data) {
+            .then(function(data) {
                 App.db.writeSetting({
                         key: 'watchlist',
                         value: data
                     })
-                    .then(function () {
+                    .then(function() {
                         App.db.writeSetting({
                             key: 'watchlist-fetched',
                             value: now.unix()
                         });
                     })
-                    .then(function () {
+                    .then(function() {
                         deferred.resolve(data || []);
                     });
             })
-            .catch(function (error) {
+            .catch(function(error) {
                 deferred.reject(error);
             });
 
