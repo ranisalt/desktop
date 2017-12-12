ca6cdd6d8536eef4edad78e6f38aa65b22a19c2a
captainyarr <captainyarr42@gmail.com>
2017-12-11 22:20:59 -0800

    Fix: Updated gulp zip function syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('zip', function () {
     gulp.src('./build/Popcorn-Time-CE/linux32/*')
         .pipe(zip('popcorn-time-ce_linux32_'+package.version+'.gz'))
         .pipe(gulp.dest('./dist'));
-    return gulp.src('./build/Popcorn-Time-CE/linux64/*',)
+    return gulp.src('./build/Popcorn-Time-CE/linux64/*')
         .pipe(zip('popcorn-time-ce_linux64_'+package.version+'.gz'))
         .pipe(gulp.dest('./dist'));
 });
