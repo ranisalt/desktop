c95fb595198b1696c91d9f9fcc2bde7fbe0daf98
captainyarr <captainyarr42@gmail.com>
2017-12-11 22:16:28 -0800

    Streamer: Increased Buffer Stream Webtorrent: Display torrent warning in the console

--- src/app/settings.js
+++ src/app/settings.js
@@ -26,6 +26,7 @@ Settings.postersJump = [134, 154, 174, 194, 214, 234, 254, 274, 294];
 Settings.alwaysFullscreen = false;
 Settings.playNextEpisodeAuto = true;
 Settings.chosenPlayer = 'local';
+Settings.bufferSize = 10 * 1024 * 1024;
 
 // Advanced UI
 Settings.alwaysOnTop = false;
